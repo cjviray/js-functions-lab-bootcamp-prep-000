cee53f9e06b881b951af71578c81a42447ec2c19
Christian V <viray.christianj@gmail.com>
2018-06-23 00:50:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
+}
